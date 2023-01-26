1c4c0b9107f09e3cffbae75957cb9488008bc849
Luis Martinez <luis_mmartinez@live.com>
2023-01-26 11:07:27 -0500

    add responsive styles to nav containers

--- src/models/quote.js
+++ src/models/quote.js
@@ -16,14 +16,16 @@ class Quote {
     quote.element.dataset.id = quote.id;
     quote.element.id = `quote-${quote.id}`;
     quote.element.innerHTML = `
-    <figure>
-      <blockquote class="blockquote">
-        <p><em><i class="bi bi-quote">${quote.body}</i></em></p>
-      </blockquote>
-      <figcaption class="blockquote-footer">
-        <cite title="Source Title">${quote.author_name}</cite>
-      </figcaption>
-    </figure>`;
+    <div id="pseudo-quote-box">
+      <figure>
+        <blockquote class="blockquote">
+          <p><em><i class="bi bi-quote">${quote.body}</i></em></p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          <cite title="Source Title">${quote.author_name}</cite>
+        </figcaption>
+      </figure>
+    </div>`;
     //   Display the quote on the DOM
     Quote.quotesContainer.innerHTML = quote.element.innerHTML;
     //   Once a quote is dsplayed,show the write a story button and activate the new story form
