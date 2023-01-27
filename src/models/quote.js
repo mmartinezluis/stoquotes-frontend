e6735ec4c824dbdc5943805356e145f01b17d0fb
Luis Martinez <luis_mmartinez@live.com>
2023-01-26 19:03:18 -0500

    remove commented code

--- src/models/quote.js
+++ src/models/quote.js
@@ -38,20 +38,8 @@ class Quote {
       quote,
       Quote.templateStyle.browse
     );
-    // `
-    // <div id="pseudo-quote-box">
-    //   <figure>
-    //     <blockquote class="blockquote">
-    //       <p><em><i class="bi bi-quote">${quote.body}</i></em></p>
-    //     </blockquote>
-    //     <figcaption class="blockquote-footer">
-    //       <cite title="Source Title">${quote.author_name}</cite>
-    //     </figcaption>
-    //   </figure>
-    // </div>`;
-    //   Display the quote on the DOM
     Quote.quotesContainer.innerHTML = quote.element.innerHTML;
-    //   Once a quote is dsplayed,show the write a story button and activate the new story form
+    // Once a quote is dsplayed, show the write a story button and activate the new story form
     writeStoryBtn.style.display = "block";
     Story.renderForm(1, quote.id);
   }
