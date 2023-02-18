9dc356d01ea3f0a4c0ef937d8cecc4b97fe32058
Luis Martinez <luis_mmartinez@live.com>
2023-02-18 14:51:57 -0500

    remove unsused code

--- src/App.js
+++ src/App.js
@@ -6,21 +6,7 @@ import StoriesMachine from "./pages/storiesMachine";
 function App() {
   return (
     <div className="App">
-      <div className="contienr">
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div className="container">
         <StoriesMachine />
         <QuotesMachine />
       </div>
