20c17d446cbdf0bae865d394c378f8704733f788
Luis Martinez <luis_mmartinez@live.com>
2023-02-22 13:54:32 -0500

    fixed syntax error in authorsSlice

--- src/app/features/authors/authorsSlice.js
+++ src/app/features/authors/authorsSlice.js
@@ -21,50 +21,41 @@ import { apiSlice } from "../api/apiSlice";
 // });
 
 // const authorsAdapter = createEntityAdapter();
-
 // const initialState = authorsAdapter.getInitialState();
-
-// export const authorsSlice = apiSlice.injectEndpoints({
+// export const extendedAuthorsApiSlice = apiSlice.injectEndpoints({
 //   endpoints: (builder) => ({
 //     getAuthors: builder.query({
 //       query: () => "/authors",
-//       transformErrorResponse: (res) => {
+//       transformResponse: (res) => {
 //         return authorsAdapter.setAll(initialState, res);
 //       },
 //     }),
 //   }),
 // });
-
-// export const { useGetAuthorsQuery } = authorsSlice;
-
-// export const selectAuthorsResult = authorsSlice.endpoints.getAuthors.select();
+// export const { useGetAuthorsQuery } = extendedAuthorsApiSlice;
+// export const selectAuthorsResult =
+//   extendedAuthorsApiSlice.endpoints.getAuthors.select();
+// const selectAuthorsData = createSelector(
+//   selectAuthorsResult,
+//   (authorsResult) => authorsResult.data
+// );
+// export const { selectAll: selectAllAuthors, selectById: selectAuthorById } =
+//   authorsAdapter.getSelectors(
+//     (state) => selectAuthorsData(state) ?? initialState
+//   );
 
 // const emptyAuthors = [];
-
 // export const selectAllAuthors = createSelector(
 //   selectAuthorsResult,
 //   (authorsResult) => authorsResult?.data ?? emptyAuthors
 // );
-
-// const selectAuthorsData = createSelector(
-//   selectAuthorsResult,
-//   (authorsResult) => authorsResult?.data ?? []
-// );
-
-// export const { selectAll: selectAllAuthors, selectById: selectAuthorById } =
-//   authorsAdapter.getSelectors((state) => {
-//     console.log("hello", state);
-//     return selectAuthorsData(state) ?? initialState;
-//   });
 
 // export const { addAuthor } = authorsSlice.actions;
 
 // export default authorsSlice.reducer;
 
 const authorsAdapter = createEntityAdapter();
-
 const initialState = authorsAdapter.getInitialState();
-
 export const extendedAuthorsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAuthors: builder.query({
@@ -75,21 +66,13 @@ export const extendedAuthorsApiSlice = apiSlice.injectEndpoints({
     }),
   }),
 });
-
 export const { useGetAuthorsQuery } = extendedAuthorsApiSlice;
-
-// Calling `someEndpoint.select(someArg)` generates a new selector that will return
-// the query result object for a query with those parameters.
-// To generate a selector for a specific query argument, call `select(theQueryArg)`.
-// In this case, the users query has no params, so we don't pass anything to select()
 export const selectAuthorsResult =
   extendedAuthorsApiSlice.endpoints.getAuthors.select();
-
 const selectAuthorsData = createSelector(
   selectAuthorsResult,
   (authorsResult) => authorsResult.data
 );
-
 export const { selectAll: selectAllAuthors, selectById: selectAuthorsById } =
   authorsAdapter.getSelectors(
     (state) => selectAuthorsData(state) ?? initialState
