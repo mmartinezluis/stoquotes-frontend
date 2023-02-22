049a3fd4f863a833754390756e27d3d2f5178062
Luis Martinez <luis_mmartinez@live.com>
2023-02-22 13:55:20 -0500

    remove unused code

--- src/app/features/authors/authorsSlice.js
+++ src/app/features/authors/authorsSlice.js
@@ -20,40 +20,6 @@ import { apiSlice } from "../api/apiSlice";
 //   },
 // });
 
-// const authorsAdapter = createEntityAdapter();
-// const initialState = authorsAdapter.getInitialState();
-// export const extendedAuthorsApiSlice = apiSlice.injectEndpoints({
-//   endpoints: (builder) => ({
-//     getAuthors: builder.query({
-//       query: () => "/authors",
-//       transformResponse: (res) => {
-//         return authorsAdapter.setAll(initialState, res);
-//       },
-//     }),
-//   }),
-// });
-// export const { useGetAuthorsQuery } = extendedAuthorsApiSlice;
-// export const selectAuthorsResult =
-//   extendedAuthorsApiSlice.endpoints.getAuthors.select();
-// const selectAuthorsData = createSelector(
-//   selectAuthorsResult,
-//   (authorsResult) => authorsResult.data
-// );
-// export const { selectAll: selectAllAuthors, selectById: selectAuthorById } =
-//   authorsAdapter.getSelectors(
-//     (state) => selectAuthorsData(state) ?? initialState
-//   );
-
-// const emptyAuthors = [];
-// export const selectAllAuthors = createSelector(
-//   selectAuthorsResult,
-//   (authorsResult) => authorsResult?.data ?? emptyAuthors
-// );
-
-// export const { addAuthor } = authorsSlice.actions;
-
-// export default authorsSlice.reducer;
-
 const authorsAdapter = createEntityAdapter();
 const initialState = authorsAdapter.getInitialState();
 export const extendedAuthorsApiSlice = apiSlice.injectEndpoints({
@@ -77,3 +43,11 @@ export const { selectAll: selectAllAuthors, selectById: selectAuthorsById } =
   authorsAdapter.getSelectors(
     (state) => selectAuthorsData(state) ?? initialState
   );
+
+// const emptyAuthors = [];
+// export const selectAllAuthors = createSelector(
+//   selectAuthorsResult,
+//   (authorsResult) => authorsResult?.data ?? emptyAuthors
+// );
+// export const { addAuthor } = authorsSlice.actions;
+// export default authorsSlice.reducer;s
